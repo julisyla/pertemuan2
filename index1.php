a12192373f51a1a60116ae0787769c98a08a3f94
juliansyah <juliansyahmuharam@gmail.com>
2022-03-13 16:09:37 +0700

    commit pertama

---
+++ index1.php
@@ -0,0 +1,58 @@
+<?php 
+// ini adalah komentar untuk satu baris
+/* 
+ini juga komentar untuk bisa banyak baris 
+*/
+// Pertemuan ke 2 - Php Dasar
+// Standar Output
+// echo, print untuk menampilkan ke layar web
+// print_r 
+// var_dump
+// contoh
+// echo "Juliansyah";
+
+// Penulisan sintax Php
+// 1.PHP di dalam HTML
+// 2.HTML di dalam PHP
+
+// Variable dan type data
+// variable
+// Tidak boleh di awali dengan angka , tapi boleh mengandung angka 
+// $nama = "Juliansyah";
+// echo "Halo, Nama saya $nama";
+
+// Operator
+// Aritmatika (+, -, *, /, %, )
+// contoh 
+// $x = 10;
+// $y = 20;
+// echo $x * $y ;
+
+// penggabung setring / concatenation /concat
+//.
+// contoh 
+// $nama_depan = "Juliansyah";
+// $nama_belakang = "Muharam";
+// echo $nama_depan ." ". $nama_belakang;
+
+// assigment (=, +=, -=, *=, /=, %=, .=, )
+    // $x = 1;
+    // $x -= 5;
+    // // echo $x;
+    // $nama = "Juliansyah";
+    // $nama .= " " ;
+    // $nama .= "muharam ";
+    // echo $nama ;
+// Perbandingan 
+    // <, >, <=, >=, ==, !=, 
+    // var_dump (1 == "1") ;
+// Identitas 
+//    ===, !== 
+    // var_dump( 1 === "1");
+// Operator Logika
+    // &&, ||, !
+    // $x = 10; 
+    // var_dump( $x < 20 && $x % 2 == 0 );
+// karna sama mempunyai nilai true kalau fungsi "and" 
+
+?>
